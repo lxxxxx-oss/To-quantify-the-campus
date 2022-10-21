72f876928928a112152b84586d694d88d180ab01
lxxxxx-oss <2238428563@qq.com>
2022-10-21 17:00:17 +0800

    完善了一卡通页面

--- components/refreshTo/space/stu-capacity/card/card.js
+++ components/refreshTo/space/stu-capacity/card/card.js
@@ -1,23 +1,65 @@
-// components/refreshTo/space/stu-capacity/card/card.js
-Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
+import * as echarts from '../../../../../ec-canvas/echarts';
 
+const app = getApp();
+
+function initChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr // new
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    title: {
+      text: '金额流动',
+      left: 'center',
+      top: '15rpx'
     },
-
-    /**
-     * 组件的初始数据
-     */
-    data: {
-
+    grid: {
+      containLabel: true
     },
+    tooltip: {
+      show: true,
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      // show: false
+    },
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
+      // show: false
+    },
+    series: [{
+      name: '金额流动',
+      type: 'line',
+      smooth: true,
+      data: [20, 36, 25, 30, 20, 5, 33]
+    }]
+  };
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
+  chart.setOption(option);
+  return chart;
+}
 
-    }
-})
+Page({
+  data: {
+    ec: {
+      onInit: initChart
+    },
+    // 余额
+    money: "5"
+  },
+
+  onReady() {
+  }
+});
