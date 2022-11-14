baa70b62428c0bb17a3718ff518070403761c599
lxxxxx-oss <2238428563@qq.com>
2022-11-15 01:01:20 +0800

    班级考勤的优化

--- components/refreshTo/space/com-capacity/attendance/attendance.js
+++ components/refreshTo/space/com-capacity/attendance/attendance.js
@@ -6,75 +6,39 @@ Page({
      * 页面的初始数据
      */
     data: {
+        // 所有学生信息
+        infoList: [],
         // 出勤率
         proportion: "",
         // 请假人数
         leaveNum: "",
+        // 请假人名单
+        leaveName: [],
         // 日期
-        date: "2022/10/21",
+        date: "",
         // 班级名单
         className: [],
-        // 是否出勤
-        isAttended: []
+        // 出勤人名单
+        isAttended: [],
+        // 未出勤人名单
+        unattended: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {      
+        // 获取当前时间
+        this.getCurrentTime()
+        // 获取信息
         this.getClassInfo()
-        // #region 
-        // 计算出勤率
-        // 班级总人数
-        // let sum = this.data.className.length
-        // // 计数器
-        // let count1 = 0
-        // let count2 = 0
-        // // 遍历整个数组对象
-        // this.data.className.forEach(e => {
-        //     if(e.status) count1 ++
-        //     if(!e.status && e.leave) count2++
-        // })
-        // // 出勤率
-        // let attendance = count1 / sum * 100
-        // // 请假人数
-        // let leave = count2
-        // // 保留两位小数
-        // attendance = attendance.toFixed(2)
-        // this.setData({
-        //     proportion: attendance,
-        //     leaveNum: leave
-        // })
-        //#endregion
+        // 计算出勤率 -> 设置延迟在数据完全请求到了之后再进行下一步
+        setTimeout(() => {
+            this.countRate()
+        }, 1000);
+        
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
@@ -91,31 +55,85 @@ Page({
     },
 
     // 获取班级信息
-    getClassInfo() {
-        var that = this  
-        return requestTwo({
-            url: '/attendance/9', 
-            method: 'GET',
-            // data: {
-            //     currentPage: 1,
-            //     pageSize: 10,
-            // }
-        }).then((res) => {
-            let infoList = res.data.data
-            console.log(infoList);
-            for (const i in infoList) {
-                console.log(i, ':', infoList[i].isAttended);
-                // console.log(i, ':', infoList[i].studentInfo.name);
-                // 存储所有学生的姓名
-                that.data.className.push(infoList[i].studentInfo.name)
-                // 存储所有学生是否出勤的标识
-                that.data.isAttended.push(infoList[i].isAttended)
-            } 
-            that.setData({
-                className: that.data.className,
-                isAttended: that.data.isAttended
-            })
-            console.log(this.data.isAttended);
+    getClassInfo() {  
+        let that = this
+        wx.request({
+            url: 'https://alaskaboo.cn/attendance/9', 
+            method: 'POST',
+            header: 'application/json',
+            data: {
+                "week": JSON.stringify(4),
+                "day": JSON.stringify(4),
+                "attend": JSON.stringify(1)
+            },
+            success(res) {
+                let infoList = res.data.data.data
+                // 拿到该对象的所有键
+                let keys = Object.keys(infoList)
+                // 遍历，访问所有数据
+                keys.forEach(e => {
+                    that.data.infoList.push(infoList[e])
+                    // console.log(that.data.infoList);
+                    // 存储学生的出勤状态
+                    if(infoList[e].status == "请假") {
+                        // 存储请假学生
+                        that.data.leaveName.push(infoList[e].studentInfo.name)
+                    }else if(infoList[e].status == "出勤") {
+                        // 存储出勤学生
+                        that.data.isAttended.push(infoList[e].studentInfo.name)
+                    }else if(infoList[e].status !== "出勤" && infoList[e].status == "请假") {
+                        // 存储未出勤学生
+                        that.data.unattended.push(infoList[e].studentInfo.name)
+                    }
+                    // 存储所有学生的名字
+                    that.data.className.push(infoList[e].studentInfo.name)
+                })
+                
+                // console.log(that.data.isAttended);
+                // console.log(that.data.className);
+                
+                // 拿到该对象的所有值
+                // console.log(Object.values(infoList));
+                that.setData({
+                    infoList: that.data.infoList,
+                    className: that.data.className,
+                    isAttended: that.data.isAttended,
+                    unattended: that.data.unattended,
+                    leaveName: that.data.leaveName
+                })
+            },
+            fail(err) {
+                console.log(err);
+            }
+        })
+    },
+
+    // 获取当前时间
+    getCurrentTime() {
+        let time = new Date()
+        let year = time.getFullYear();
+        let month = time.getMonth() + 1 < 10 ? '0' + (time.getMonth() + 1) : (time.getMonth() + 1);
+        let day = time.getDate() < 10 ? '0' + time.getDate() : time.getDate();
+        this.setData({
+            date: year + '/' + month + '/' + day
+        })
+    },
+
+    // 计算出勤率
+    countRate() {
+        let that = this
+        // 计算出勤率
+        // 班级总人数
+        let sum = that.data.className.length
+        // 出勤率
+        let attendance = that.data.isAttended.length / sum * 100
+        // 请假人数
+        let leave = that.data.leaveName.length
+        // 保留两位小数
+        attendance = attendance.toFixed(1)
+        this.setData({
+            proportion: attendance,
+            leaveNum: leave
         })
     }
 })
